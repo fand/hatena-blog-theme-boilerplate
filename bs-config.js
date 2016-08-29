b693b5ea4376284b0bf68dfedebff704a7fe895b
amagitakayosi <fand.gmork@gmail.com>
2016-08-29 23:41:14 +0900

    Install browserSync

---
+++ bs-config.js
@@ -0,0 +1,22 @@
+
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+    files: ['index.css'],
+    server: true,
+    open: false,
+    socket: {
+        domain: 'localhost:3000'
+    },
+};
